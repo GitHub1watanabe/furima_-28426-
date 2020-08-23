39e6c7d6614aa0727d1a33176871af003d97a2a2
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-08-23 20:04:38 +0900

    ユーザー登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+         validates :password, length: { minimum: 6 }
+         validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
 end
