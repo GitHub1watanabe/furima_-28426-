8b66105d9e8dceb932ce8004e310cb0cb2e24438
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-08-26 16:11:42 +0900

    テストコード異常系

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
          validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
          validates :password, length: { minimum: 6 }
-         validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+         validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name              {"わたなべ"}
+    name              {"ああああああああああ"}
     email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {password}
+    first_name          {"ああああああ"}
+    last_name           {"ああああああ"}
+    first_name_kana     {"ああああああああああ"}
+    last_name_kana      {"ああああああああああ"}
+    birth_date           {"Fri, 15 May 1981"}
   end
 end
