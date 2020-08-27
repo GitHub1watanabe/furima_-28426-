f05c194c60efdb8a036f996074d0195c20e80eab
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-08-27 19:33:49 +0900

    ログインしているユーザーだけが、出品ページへ遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:new]
   def index
   end
-  new
+  def new
+  end
+  private
+  def move_to_index
+    unless user_signed_in?
+      flash[:alert]="You need to sign in or sign up before continuing."
+      redirect_to "/users/sign_in"
+    end
+  end
 end
