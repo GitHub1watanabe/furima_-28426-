f091e48de880e1244b3a07eefde015107f975166
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-08-28 20:07:55 +0900

    配送料金負担の記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
   belongs_to_active_hash :status
+  belongs_to_active_hash :fee
   has_one_attached :image
   belongs_to :user
 end
