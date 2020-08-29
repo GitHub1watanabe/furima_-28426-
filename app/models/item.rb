025a4033eea01743e703c11786a33ac910a93ff0
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-08-29 16:09:52 +0900

    配送日時についての記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :fee
   belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :delivery_date
   has_one_attached :image
   belongs_to :user
 end
