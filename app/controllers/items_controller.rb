585fe98a3b1749d02a5509e720944a7effb70e79
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-08-29 17:45:21 +0900

    商品出品機能　コントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,20 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   private
+  def item_params
+    params.require(:item).permit(:image, :name, :comment, :category_id, :status_id, :fee_id, :delivery_area_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
   def move_to_index
     unless user_signed_in?
       flash[:alert]="You need to sign in or sign up before continuing."
