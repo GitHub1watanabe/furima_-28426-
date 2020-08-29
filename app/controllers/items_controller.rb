82de6f0442e7a6888efcf65296bfb28c403b2fbc
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-08-29 20:25:24 +0900

    からの入力時にエラー文を出す記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,21 +2,29 @@ class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new]
   def index
   end
+
   def new
+    # binding.pry
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.valid?
+       @item.save
       redirect_to root_path
     else
       render :new
     end
   end
+
+
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :comment, :category_id, :status_id, :fee_id, :delivery_area_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
+
   def move_to_index
     unless user_signed_in?
       flash[:alert]="You need to sign in or sign up before continuing."
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre
-  belongs_to_active_hash :status
-  belongs_to_active_hash :fee
-  belongs_to_active_hash :delivery_area
-  belongs_to_active_hash :delivery_date
-  has_one_attached :image
-  belongs_to :user
+ 
+    validates :image, presence:  { message:'Please paste the image'}
+    validates :name, presence:{ message:'Please enter the product name'}
+    validates :comment, presence:{ message:'Please enter the product description'}
+    validates :category_id, presence:{ message:'Please select a category'}
+    validates :status_id, presence:{ message:'Please select the product status'}
+    validates :fee_id, presence:{ message:'Please select a shipping fee'}
+    validates :delivery_area_id,  presence:{ message:'Please select a delivery area'}
+    validates :delivery_date_id,  presence:{ message:'Please select a delivery date'}
+    validates :price, presence: { message:'Enter price'}
+  
+
+    belongs_to_active_hash :genre
+    belongs_to_active_hash :status
+    belongs_to_active_hash :fee
+    belongs_to_active_hash :delivery_area
+    belongs_to_active_hash :delivery_date
+    has_one_attached :image
+    belongs_to :user
 end
