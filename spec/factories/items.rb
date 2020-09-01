391a4016a32972a9bf66cd945a1f740feed6ff00
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-09-01 16:36:57 +0900

    テストコード正常系の記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name              {"ああああああああ"}
+    price                 {"1000"}
+    comment              {"あああああああ"}
+    category_id          {1}
+    status_id           {1}
+    delivery_date_id     {2}
+    delivery_area_id       {2}
+    fee_id           {1}
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
+describe Item do
+  before do
+    # user = FactoryBot.create(:item_user)
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('public/images/test_image.jpg')
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザーの商品出品' do
+    context '出品のページ遷移がうまくいくとき' do
+      it "出品のページ遷移がうまくいくとき" do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
