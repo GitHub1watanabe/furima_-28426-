7bfbecd3526ada9f37e0584ba517d3b2ba5c071c
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-09-04 14:15:33 +0900

    商品一覧表示機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new]
   def index
-    @items = Item.all
+    @items = Item.order("created_at ASC")
   end
 
   def new
