750080fdc2f428c664d1a2c15800d823eeb187d8
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-09-04 16:10:47 +0900

    orderメソッドを用いて昇順にした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new]
   def index
-    @items = Item.order("created_at ASC")
+      @items = Item.order("created_at ASC")
   end
 
   def new
