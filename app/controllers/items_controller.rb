89e4cd8f186287eba1895f3a2033d0d67d17dd13
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-09-11 19:12:42 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
       @items = Item.all.order("created_at ASC")
   end
@@ -15,6 +15,11 @@ class ItemsController < ApplicationController
 
   def edit
     
+  end
+
+  def destroy
+      @item.destroy
+      redirect_to root_path
   end
 
   def update
