a294faae32ba7a050a8aca55a4f83bba610efe3d
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-09-17 17:30:01 +0900

    クレジット決済処理

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../item_price")
 require('jquery')
+require("../card")    //追加
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
