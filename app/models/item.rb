1845b6525cf504807ab435685f7ee5cd96be3ade
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-10-15 18:56:47 +0900

    コミット7

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
     validates :image, presence:  { message:'Please paste the image'}
     validates :name, presence:{ message:'Please enter the product name'}
     validates :comment, presence:{ message:'Please enter the product description'}
-    validates :category_id, presence:{ other_than: 0, message:'Please select a category'}
-    validates :status_id, presence:{ other_than: 0, message:'Please select the product status'}
-    validates :fee_id, presence:{ other_than: 0, message:'Please select a shipping fee'}
-    validates :delivery_area_id,  presence:{ other_than: 0, message:'Please select a delivery area'}
-    validates :delivery_date_id,  presence:{ other_than: 0, message:'Please select a delivery date'}
+    validates :category_id, numericality:{ other_than: 0, message:'Please select a category'}
+    validates :status_id, numericality:{ other_than: 0, message:'Please select the product status'}
+    validates :fee_id, numericality:{ other_than: 0, message:'Please select a shipping fee'}
+    validates :delivery_area_id,  numericality:{ other_than: 0, message:'Please select a delivery area'}
+    validates :delivery_date_id,  numericality:{ other_than: 0, message:'Please select a delivery date'}
     validates :price, presence: { message:'Enter price'}
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
